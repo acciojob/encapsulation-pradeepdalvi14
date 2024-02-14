74405ffb503aee9719cca54cd84916e4d0e9c035
Pradeep <pradeepd6915@gmail.com>
2024-02-14 15:50:46 +0530

    geteer and setter added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+      RWOnly obj = new RWOnly();
+      obj.setName("pradeep");
+      System.out.println(obj.getName());
+
+      // Error: java: a has private access in com.driver.RWOnly
+  }
 }
